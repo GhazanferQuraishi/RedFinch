d75720f4fe7c1abd7a8399612db2287b83faf28c
GhazanferQuraishi <ghazanfersq@gmail.com>
2024-02-18 17:58:22 +0530

    aa

---
+++ .next/static/webpack/pages/index.9320dd894b7c239f.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/index",{
+
+/***/ "./src/data/intro3.json":
+/*!******************************!*\
+  !*** ./src/data/intro3.json ***!
+  \******************************/
+/***/ (function(module) {
+
+module.exports = JSON.parse('[{"id":1,"title":{"first":"Get your Dream","second":" Kitchen Today!"},"content":"","image":"/assets/img/slid/n/slider1.jpg"},{"id":2,"title":{"first":"Elevate your space","second":" with elegant interiors"},"content":"","image":"/assets/img/slid/n/slider2.jpg"},{"id":3,"title":{"first":"Some people look for a beautiful ","second":"place others make a place beautiful"},"content":"","image":"/assets/img/slid/n/slider-3.jpg"}]');
+
+/***/ })
+
+});
